80b9ea93c56f1033aaf668a6c3462a54cf973ad4
qiaofei <1216039873@qq.com>
2019-08-16 17:09:11 +0800

    spring in action 4.0 chapter 5  spittr

---
+++ src/main/java/spittr/web/WebConfig.java
@@ -0,0 +1,42 @@
+package spittr.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author qiaofei@css.com.cn
+ * @version 1.0
+ * @description
+ * @createTime 15:21 2019/8/15
+ * @modifyTime
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("spittr.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		super.addResourceHandlers(registry);
+	}
+}
